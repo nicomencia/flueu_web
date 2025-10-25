cc4d138f695cd7e25d2b9843eff46253ac435c21
nicomencia <47212402+nicomencia@users.noreply.github.com>
2025-10-25 20:07:02 +0200

    Updated Hero.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ export default function Hero() {
           </a>
           <a href="https://vinted.com/fleuestudio" target="_blank" rel="noopener noreferrer" aria-label="Vinted">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/>
+              <path d="M19.5 6L12 18L4.5 6h15z"/>
             </svg>
           </a>
         </div>
